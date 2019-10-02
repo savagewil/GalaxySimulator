5fb4dfccf67357659905997299d490ccb22b2cbf
savagewil <wxs5474@rit.edu>
2019-10-01 21:02:46 -0400

    A galaxy on a torus

--- Simulator_Vectorized.py
+++ Simulator_Vectorized.py
@@ -1,9 +1,10 @@
-# import pygame
+import pygame
 import random
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 # import Calculations
 # import Mass
@@ -27,29 +28,30 @@ screensize = 500
 #         # print 1
 #         mass1.combine(mass2)
 #
-# pygame.init()
-# pygame.key.set_repeat(50, 10)
-# screen = pygame.display.set_mode([screensize, screensize])
-
-count = 100
+pygame.init()
+pygame.key.set_repeat(50, 10)
+screen = pygame.display.set_mode([screensize, screensize])
+zoom = 1000
+count = 1000
 generations = 0
 radius = 1.702931e+21
 radius = 1.702931e+7
-G = 6.67408e-11
-time_step = 200
+limit = radius * 1000
+G = -6.67408e-11
+time_step = 10e-1
 time_step_2 = ((time_step) ** 2) / 2
-display_rate = 10000
-time_limit = 100000
+display_rate = 1
+time_limit = 100000000
 # Time = 3.154e13
-# initialVelocity = 2e10
+initialVelocity = 500*2
 
 radius /= 2
 
-zoom = radius * 4
+# zoom = radius * 4
 
-masses = (np.random.randn(100, 1) + 0.5) * (2e25 - 2e24) + 2e24
-positions = np.random.randn(100, 2) * radius
-velocities = np.zeros((100, 2))
+masses = (np.random.rand(count, 1)) * (2e25 - 2e24) + 2e24
+positions = np.random.rand(count, 2) * radius
+velocities = (np.random.rand(count, 2) - .05) * initialVelocity
 
 epsilon = 0.0000000001
 
@@ -75,49 +77,75 @@ def gravity(masses, diff, distance_squared):
     # print(diff.shape)
     # print(distance_squared.shape)
     # print("==================")
-    directions = np.divide(diff, np.reshape(distance_squared, (count, count, 1)) + epsilon)
+    directions = np.divide(diff, np.reshape(np.sqrt(distance_squared), (count, count, 1)) + epsilon)
+    # print("DIFF", diff)
+    # print("distance_squared", distance_squared)
+    # print("directions", directions)
+    # print("dis", np.square(directions)[:,:,0] + np.square(directions)[:,:,1])
     # print(directions.shape)
     # print(force.shape)
-    # print(masses.shape)
+    # print("masses",masses)
     # print("==================")
-    force_d = np.negative(np.multiply(directions, np.reshape(force, (count, count, 1))))
+    # print("force", force)
+    # print("force_reshape", np.reshape(force, (count, count, 1)))
+    # print("dir", directions)
+    force_d = np.multiply(directions, np.reshape(force, (count, count, 1)))
+    # print("f_d", force_d)
+    # force_d = np.multiply(np.ones((count, count)) - np.eye(count), force_d)
+    # print("f_d_middle", force_d)
     mass = np.divide(1, masses)
 
-    accelerations = np.zeros((100, 2))
+    accelerations = np.zeros((count, 2))
     accelerations[:, :1] = np.dot(force_d[:, :, 0], mass)
+    # print("accelerations_1", accelerations)
     accelerations[:, 1:2] = np.dot(force_d[:, :, 1], mass)
+    # print("accelerations_2", accelerations)
     # print(accelerations.shape)
-    return accelerations
+    return accelerations, directions
 
 
 def move(positions, velocities, accelerations, time_step, time_step_2):
-    positions = np.add(np.add(positions, np.multiply(time_step, velocities)), np.multiply(time_step_2, accelerations))
-    velocities = np.multiply(time_step, accelerations)
+    positions = np.mod(np.add(np.add(positions, np.multiply(time_step, velocities)), np.multiply(time_step_2, accelerations)) + limit/2, limit) - limit/2
+    velocities = np.add(velocities,np.multiply(time_step, accelerations))
     return positions, velocities
 
 
-# def draw(screen, positions):
-#     screen_positions = (positions * screensize / 2 + screensize / 2).astype(int)
-#     map(lambda position:pygame.draw.circle(screen,
-#                        [150, 150, 150],
-#                        position,
-#                        10), screen_positions)
+def draw(screen, positions, directions):
+    screen_positions = ((positions) * screensize / (radius * 2 * zoom) + screensize/2).astype(int)
+    screen_dir = ((positions) * screensize / (radius * 2 * zoom) + directions * 20 + screensize/2).astype(int)
+    # print(screen_positions[0])
+    for i in range(len(screen_positions)):
+        pygame.draw.circle(screen, [150, 150, 150], screen_positions[i], 2)
+        # print(screen_dir[i])
+        # for j in range(len(screen_positions)):
+        #     pygame.draw.circle(screen, [200, 0, 0], screen_dir[i, j], 2)
 
+acceleration_on = True
 
 while generations < time_limit:
     diff = get_diff(positions)
     distance_squared = get_distance_squared(diff)
-    accelerations = gravity(masses, diff, distance_squared)
+    accelerations, directions = gravity(masses, diff, distance_squared)
+    accelerations = accelerations * acceleration_on
     positions, velocities = move(positions, velocities, accelerations, time_step, time_step_2)
     # print(positions.shape)
     # print(positions[:, 1].shape)
 
     if generations % display_rate == 0:
-        print(positions[0])
-        fig = sns.scatterplot(x=positions[:, 0], y=positions[:, 1])
-        plt.ylim(-radius, radius)
-        plt.xlim(-radius, radius)
-        plt.show()
+        # print("Render")
+        # print(accelerations[0])
+        # print(accelerations[1])
+        screen.fill([0, 0, 0])
+        draw(screen, positions, directions)
+        pygame.display.update()
+        # pygame.time.delay(10)
+        # input()
+        # print(positions[0])
+        # fig = sns.scatterplot(x=positions[:, 0], y=positions[:, 1])
+        # plt.ylim(-radius, radius)
+        # plt.xlim(-radius, radius)
+        # plt.show()
     generations += 1
+    events = pygame.event.get()
     # input()
 print(generations)
